944b32c4b30ccc0ff2070be453454754b9e24b3e
rupali-barla-au16 <81455775+rupali-barla-au16@users.noreply.github.com>
2021-08-25 21:39:51 +0530

    Fix-Update-Profile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -115,6 +115,7 @@ export const userDeleteReducer = (state = {}, action) => {
   }
 }
 
+
 export const userUpdateReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_UPDATE_REQUEST:
